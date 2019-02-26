dcf79a77f37570814cc13bd45db68fdb6262fc62
Ryo Suzuki <ryosuzk@gmail.com>
2019-02-26 15:11:50 -0700

    Finish testing interface

--- test/server.js
+++ test/server.js
@@ -4,6 +4,7 @@ const path = require('path')
 const socketio = require('socket.io')
 const app = express()
 const bodyParser = require('body-parser')
+const dgram = require('dgram')
 
 app.use(bodyParser.json())
 app.use('/', express.static(__dirname))
@@ -20,8 +21,54 @@ const io = socketio(server)
 io.on('connection', (socket) => {
   console.log('socket connected')
 
-  socket.on('message', (message) => {
-    console.log(message)
+  socket.on('move', (data) => {
+    console.log(data)
+    const client = dgram.createSocket('udp4')
+    let forward = { a1: 0, a2: 1, b1: 1, b2: 0, duration: 200 }
+    let backward = { a1: 1, a2: 0, b1: 0, b2: 1, duration: 200 }
+    let right = { a1: 0, a2: 1, b1: 0, b2: 1, duration: 200 }
+    let left = { a1: 1, a2: 0, b1: 1, b2: 0, duration: 200 }
+    let json = JSON.parse(data)
+    let ip = json.ip
+    let dir = json.dir
+    let command
+    switch (dir) {
+      case 'forward':
+        command = forward
+        break
+      case 'backward':
+        command = backward
+        break
+      case 'left':
+        command = left
+        break
+      case 'right':
+        command = right
+        break
+    }
+    let str = JSON.stringify(command)
+    let message = Buffer.from(str)
+    let port = 8883
+    client.send(message, 0, message.length, port, ip, function(err, bytes) {
+      if (err) throw err
+      client.close()
+    })
+  })
+
+  socket.on('actuate', (data) => {
+    console.log(data)
+    const client = dgram.createSocket('udp4')
+    let json = JSON.parse(data)
+    let ip = json.ip
+    let pos = json.pos
+    let command = { pos: pos }
+    let str = JSON.stringify(command)
+    let message = Buffer.from(str)
+    let port = 8883
+    client.send(message, 0, message.length, port, ip, function(err, bytes) {
+      if (err) throw err
+      client.close()
+    })
   })
 
 })
